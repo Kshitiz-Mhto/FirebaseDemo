791e5bb97bd734bcb4887a5c45f9704f43f8c8be
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-28 17:40:15 +0545

    inserting done in realtime database in firebasae

--- app/src/main/java/com/wisewords/firebaseauthdemo/SignupFragment.kt
+++ app/src/main/java/com/wisewords/firebaseauthdemo/SignupFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.wisewords.firebaseauthdemo.databinding.FragmentSignupBinding
+import com.wisewords.firebaseauthdemo.modal.Army
 
 class SignupFragment : Fragment() {
+
+    private lateinit var dbRef: DatabaseReference
 
     private lateinit var binding: FragmentSignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +27,14 @@ class SignupFragment : Fragment() {
     ): View? {
 
         binding = FragmentSignupBinding.inflate(inflater, container, false)
+        dbRef = FirebaseDatabase.getInstance().getReference("Army")
+
+        binding.btnSignup.setOnClickListener {
+            saveMyArmy()
+            findNavController().navigate(
+                R.id.action_signupFragment_to_loginFragment
+            )
+        }
 
         binding.btnLoginFromSignUp.setOnClickListener {
             findNavController().navigate(
@@ -28,6 +42,40 @@ class SignupFragment : Fragment() {
             )
         }
 
+
         return binding.root
     }
+
+    private fun saveMyArmy() {
+        // getting the values
+
+        val armyFirstName = binding.etFillFirstName.text.toString()
+        val armyLastName = binding.etFillLastName.text.toString()
+        val armyEmail = binding.etFillEmail.text.toString()
+        val armyPassword = binding.etFillPassword.text.toString()
+        val armyPassword1 = binding.etPasswordAgain.text.toString()
+        val armyPolicy = binding.chkAgreement.isChecked
+        val armyRobot = binding.chkRobot.isChecked
+
+        if (armyFirstName.isEmpty() or armyLastName.isEmpty()){
+            binding.etFillLastName.error = "enter your fucking name"
+            binding.etFillFirstName.error = "enter your fucking name"
+        }
+
+        if (armyEmail.isEmpty()){
+            binding.etFillEmail.error = "enter your fucking email"
+        }
+
+        val armyId = dbRef.push().key!!
+        val army = Army(armyId, armyFirstName,armyLastName, armyEmail, armyPassword, armyPassword1, armyPolicy, armyRobot)
+        dbRef.child(armyId).setValue(army)
+            .addOnCompleteListener {
+                Toast.makeText(this.context, "data is inserted", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener{ err->
+                Toast.makeText(this.context, "Error!!", Toast.LENGTH_LONG).show()
+
+            }
+
+    }
+
 }
--- app/src/main/java/com/wisewords/firebaseauthdemo/modal/Army.kt
+++ app/src/main/java/com/wisewords/firebaseauthdemo/modal/Army.kt
@@ -1,3 +1,12 @@
 package com.wisewords.firebaseauthdemo.modal
 
-data class Army()
+data class Army(
+    val armyId: String,
+    val armyFirstName: String,
+    val armyLastName: String,
+    val armyEmail: String,
+    val armyPassword: String,
+    val armyPassword1: String,
+    val armyPolicy: Boolean,
+    val armyRobot: Boolean
+    )
